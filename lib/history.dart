02bc5142ed770509f674c9feff0a335e0f2b1f88
scotch115 <jordangamache115@gmail.com>
2021-01-01 23:05:13 -0500

    Fixed Firestore integration and upgraded history page for Firestore

--- lib/history.dart
+++ lib/history.dart
@@ -2,10 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'main.dart';
-
 class History extends StatefulWidget {
-  @override
   @override
   _HistoryState createState() => _HistoryState();
 }
@@ -14,22 +11,183 @@ class _HistoryState extends State<History> {
   List<Widget> results = [];
   final databaseReference = Firestore.instance;
 
-  // --------------------------- Class Variables --------------------------- //
-
   @override
   void initState() {
-    // TODO: implement initState
-    getData();
     super.initState();
+    getData();
   }
+  // --------------------------- Class Variables --------------------------- //
 
-  void getData() {
+  Future<void> getData() async {
     databaseReference
         .collection('rotarySelection')
         .getDocuments()
         .then((QuerySnapshot snapshot) {
-      snapshot.documents.forEach((element) => print(
-          '${element.data}')); // Once Firestore is properly configured, this will be updated to generate a WidgetList based on Firestore document data
+      snapshot.documents.forEach((element) {
+        String input =
+            element.data.values.toString().replaceAll(new RegExp(r"\s+"), "");
+        print(input);
+        if (input.length > 10) {
+          results.add(Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    width: 50,
+                    height: 50,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        boxShadow: <BoxShadow>[
+                          BoxShadow(
+                              color: Colors.black54,
+                              blurRadius: 15.0,
+                              offset: Offset(0.0, 0.75))
+                        ],
+                        shape: BoxShape.circle,
+                        color: Color.fromRGBO(125, 202, 160, 1)),
+                    child: Text(
+                      (input != null) ? input[1] : "null",
+                      style: TextStyle(color: Colors.white, fontSize: 32),
+                    )),
+                Text(
+                  input[2],
+                  style: TextStyle(color: Colors.white, fontSize: 32),
+                ),
+                Container(
+                    width: 50,
+                    height: 50,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        boxShadow: <BoxShadow>[
+                          BoxShadow(
+                              color: Colors.black54,
+                              blurRadius: 15.0,
+                              offset: Offset(0.0, 0.75))
+                        ],
+                        shape: BoxShape.circle,
+                        color: Color.fromRGBO(125, 202, 160, 1)),
+                    child: Text(
+                      input[3],
+                      style: TextStyle(color: Colors.white, fontSize: 32),
+                    )),
+                Text(
+                  input[4],
+                  style: TextStyle(color: Colors.white, fontSize: 32),
+                ),
+                Container(
+                    width: 50,
+                    height: 50,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        boxShadow: <BoxShadow>[
+                          BoxShadow(
+                              color: Colors.black54,
+                              blurRadius: 15.0,
+                              offset: Offset(0.0, 0.75))
+                        ],
+                        shape: BoxShape.circle,
+                        color: Color.fromRGBO(96, 136, 175, 1)),
+                    child: Text(input[5],
+                        style: TextStyle(color: Colors.white, fontSize: 32))),
+                Text(
+                  input[6],
+                  style: TextStyle(color: Colors.white, fontSize: 32),
+                ),
+                Container(
+                    width: 50,
+                    height: 50,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        boxShadow: <BoxShadow>[
+                          BoxShadow(
+                              color: Colors.black54,
+                              blurRadius: 15.0,
+                              offset: Offset(0.0, 0.75))
+                        ],
+                        shape: BoxShape.circle,
+                        color: Color.fromRGBO(96, 136, 175, 1)),
+                    child: Text(input[7],
+                        style: TextStyle(color: Colors.white, fontSize: 32))),
+                Text(
+                  "=",
+                  style: TextStyle(color: Colors.white, fontSize: 32),
+                ),
+                Container(
+                    width: 50,
+                    height: 50,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        boxShadow: <BoxShadow>[
+                          BoxShadow(
+                              color: Colors.black54,
+                              blurRadius: 15.0,
+                              offset: Offset(0.0, 0.75))
+                        ],
+                        shape: BoxShape.circle,
+                        color: Color.fromRGBO(96, 136, 175, 1)),
+                    child: Text("24",
+                        style: TextStyle(color: Colors.white, fontSize: 32)))
+              ]));
+        } else {
+          results.add(Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                    width: 50,
+                    height: 50,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          color: Colors.black54,
+                          blurRadius: 15.0,
+                          offset: Offset(0.0, 0.75))
+                    ], shape: BoxShape.circle, color: Colors.red),
+                    child: Text(
+                      input[1],
+                      style: TextStyle(color: Colors.white, fontSize: 32),
+                    )),
+                Container(
+                    width: 50,
+                    height: 50,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          color: Colors.black54,
+                          blurRadius: 15.0,
+                          offset: Offset(0.0, 0.75))
+                    ], shape: BoxShape.circle, color: Colors.red),
+                    child: Text(
+                      input[2],
+                      style: TextStyle(color: Colors.white, fontSize: 32),
+                    )),
+                Container(
+                    width: 50,
+                    height: 50,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          color: Colors.black54,
+                          blurRadius: 15.0,
+                          offset: Offset(0.0, 0.75))
+                    ], shape: BoxShape.circle, color: Colors.red),
+                    child: Text(input[3],
+                        style: TextStyle(color: Colors.white, fontSize: 32))),
+                Container(
+                    width: 50,
+                    height: 50,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(boxShadow: <BoxShadow>[
+                      BoxShadow(
+                          color: Colors.black54,
+                          blurRadius: 15.0,
+                          offset: Offset(0.0, 0.75))
+                    ], shape: BoxShape.circle, color: Colors.red),
+                    child: Text(input[4],
+                        style: TextStyle(color: Colors.white, fontSize: 32))),
+              ]));
+        }
+      });
     });
   }
 
@@ -49,45 +207,51 @@ class _HistoryState extends State<History> {
                     image: AssetImage("assets/background.png"),
                     fit: BoxFit.cover)),
             child: Scaffold(
-              appBar: AppBar(
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-                leading: Container(
-                    padding: EdgeInsets.only(left: 10),
-                    child: FloatingActionButton(
-                      child: Text(
-                        "X",
-                        style: TextStyle(fontSize: 32),
-                      ),
-                      backgroundColor: Color.fromRGBO(138, 131, 222, 1),
-                      onPressed: () => Navigator.of(context).pop(true),
-                    )),
-              ),
-              backgroundColor: Colors.transparent,
-              body: Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // Display history in chronological order
-                    // Need to implement calls to Firestore, and method to filter passing and failing expressions to properly display each
-                    Center(
+                appBar: AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  leading: Container(
+                      padding: EdgeInsets.only(left: 10),
+                      child: FloatingActionButton(
                         child: Text(
-                      "History",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 32,
-                          color: Colors.white),
-                    )),
-                    Column(
-                        // children: (widget.inputResults != null)
-                        //     ? widget.inputResults
-                        //     : [Container()],
-                        // style: TextStyle(color: Colors.white, fontSize: 20),
-                        )
-                  ],
+                          "X",
+                          style: TextStyle(fontSize: 32),
+                        ),
+                        backgroundColor: Color.fromRGBO(138, 131, 222, 1),
+                        onPressed: () => Navigator.of(context).pop(true),
+                      )),
                 ),
-              ),
-            )));
+                backgroundColor: Colors.transparent,
+                body: StreamBuilder(
+                  stream: Firestore.instance
+                      .collection('rotarySelection')
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return CircularProgressIndicator(
+                        value: snapshot.connectionState.index.toDouble(),
+                      );
+                    }
+                    return Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // Display history in chronological order
+                          // Need to implement calls to Firestore, and method to filter passing and failing expressions to properly display each
+                          Center(
+                              child: Text(
+                            "History",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 32,
+                                color: Colors.white),
+                          )),
+                          Column(children: results)
+                        ],
+                      ),
+                    );
+                  },
+                ))));
   }
 }
